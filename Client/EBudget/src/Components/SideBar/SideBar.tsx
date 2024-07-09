a7c0ca4df22357f247666a425db6d857a13ab368
kiokoeric <kiokoerick040@gmail.com>
2024-07-09 15:50:30 +0300

    Creation of the GetUserID Hook and adding the tailwind CSS design configurations to the EBudget Sidebar in the EBudget Website

--- Client/EBudget/src/Components/SideBar/SideBar.tsx
+++ Client/EBudget/src/Components/SideBar/SideBar.tsx
@@ -7,6 +7,7 @@ import { AiOutlineUserAdd } from "react-icons/ai";
 import { BiSolidDashboard } from "react-icons/bi";
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from "react-router-dom";
+import { useGetUserID } from "../Hooks/useGetUserID";
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { faPiggyBank } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -17,7 +18,7 @@ import { faMoneyBillTransfer } from '@fortawesome/free-solid-svg-icons';
 const SideBar: React.FC = () => {
 
     const [Name, setName] = useState("")
-    // const userID = useGetUserID();
+    const userID = useGetUserID();
 
     useEffect(() => {
         
@@ -47,48 +48,48 @@ const SideBar: React.FC = () => {
     }
 
 return (
-    <div>
-        <section>
+    <div id="SideBar" className="bg-green-800 flex flex-col justify-between py-5 rounded text-white w-56" >
+        <section className="flex flex-col items-center gap-3" >
             <div>
-                <figure>
-                    <FontAwesomeIcon icon={faUser} />
-                    <h1>EBudget</h1>
+                <figure className="flex font-bold items-center justify-center gap-1.5">
+                    <FontAwesomeIcon icon={faUser} className="bg-white text-green-700 p-3 rounded-full text-3xl" />
+                    <h1 className="text-4xl" >EBudget</h1>
                 </figure>
                 {userID ? <h3>Welcome {Name}</h3> : null }
             </div>
-            <div>
-                <Link to="/Dashboard" className='Link' >
+            <div className="flex flex-col gap-3.5 mt-10 px-5">
+                <Link to="/Dashboard" className='flex gap-1.5 text-lg'>
                     < BiSolidDashboard className='ReactIcon' />
                     Dashboard
                 </Link>
-                <Link to="/Income" className='Link' >
+                <Link to="/Income" className='flex gap-1.5 text-lg'>
                     <FontAwesomeIcon icon={faMoneyBillTrendUp} />
                     Incomes
                 </Link>
-                <Link to="/Expense" className='Link' >
+                <Link to="/Expense" className='flex gap-1.5 text-lg'>
                     <FontAwesomeIcon icon={faMoneyBillTransfer} />
                     Expenses
                 </Link>
-                <Link to="/Savings" className='Link' >
+                <Link to="/Savings" className='flex gap-1.5 text-lg'>
                     <FontAwesomeIcon icon={faPiggyBank} />
                     Savings & Investments
                 </Link>
-                <Link to="/Loan_Calculator" className='Link' >
+                <Link to="/Loan_Calculator" className='flex gap-1.5 text-lg' >
                     <FontAwesomeIcon icon={faBuildingColumns} />
                     Loan Calculator
                 </Link>
             </div>
         </section>
-        <section>
-            <div>
+        <section  className="flex flex-col gap-5 px-5">
+            <div className="flex flex-col gap-3.5" >
                 {
-                userID ? <Link to={`/${userID}`} className='Link' >
+                userID ? <Link to={`/${userID}`} className='flex gap-1.5 text-lg' >
                     <AiOutlineUser className='ReactIcon' />
                     Profile
                 </Link> : ""
                 }
                 {
-                !userID ? <Link to="/Registration" className='Link' >
+                !userID ? <Link to="/Registration" className='flex gap-1.5 text-lg' >
                     <AiOutlineUserAdd className='ReactIcon' />
                         Sign Up
                     </Link> : ""
@@ -96,13 +97,13 @@ return (
                 {
                 !Cookie.auth_token ?
                 (
-                    <Link to="/" className='Link' >
+                    <Link to="/" className='flex gap-1.5 text-lg' >
                         <FiLogIn className='ReactIcon' />
                         Login
                     </Link>
                 ) : 
                 (
-                    <button onClick={Logout} className='Logout'>
+                    <button onClick={Logout} className='flex gap-1.5 text-lg'>
                         <BiLogOut className="ReactIcon" />
                         Logout
                     </button>
