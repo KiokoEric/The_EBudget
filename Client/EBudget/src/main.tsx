974c0582b9e41f473ec1d2a598e932fa0e2edc39
kiokoeric <kiokoerick040@gmail.com>
2024-07-09 12:35:41 +0300

    Creation of the EBudget Web Application

--- Client/EBudget/src/main.tsx
+++ Client/EBudget/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './index.css';
+import App from './App.tsx';
+import ReactDOM from 'react-dom/client';
+import { SnackbarProvider } from 'notistack';
+import { BrowserRouter } from "react-router-dom";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons'; // fas: Font Awesome Solid
+
+// Add the imported icons to the library
+library.add(fas);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <SnackbarProvider>
+      <App />
+    </SnackbarProvider>
+  </BrowserRouter>
 )
