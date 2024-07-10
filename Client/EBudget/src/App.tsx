a058421d2da17c08d92b8f4c77b3c494b11d5a24
kiokoeric <kiokoerick040@gmail.com>
2024-07-10 16:27:34 +0300

    Creation of the Income Page in the Educate Website

--- Client/EBudget/src/App.tsx
+++ Client/EBudget/src/App.tsx
@@ -1,12 +1,20 @@
 import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import SideBar from './Components/SideBar/SideBar';
+const IncomePage = React.lazy(() => import('./Pages/Income/Income'))
 
 function App() {
 
   return (
-    <>
-      <SideBar />
-    </>
+    <div className='flex gap-5 m-auto w-full'>
+      <section>
+        <SideBar />
+      </section>
+      <Routes>
+        <Route path='/Income' element={ <React.Suspense><IncomePage /> </React.Suspense> }  />
+      </Routes>
+    </div>
   )
 }
 
---
+++ Client/EBudget/src/Pages/Income/Income.tsx
@@ -0,0 +1,137 @@
+import * as z from 'zod';
+import Axios from "axios";
+import React, { useEffect, useState } from 'react';
+import { useCookies } from "react-cookie";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import Button from '../../Components/Common/Button/Button';
+import Output from '../../Components/Common/Output/Output';
+import Heading from '../../Components/Common/Heading/Heading';
+import { useGetUserID } from "../../Components/Hooks/useGetUserID";
+
+interface FormValues {
+    Description: string;
+    Amount: number;
+    Date: Date;
+    Memo: string;
+};
+
+const Income:React.FC = () => {
+
+    const userID = useGetUserID()
+    const [userOwner, setuserOwner] = useState(userID)
+    const [ Success, setSuccess ] = useState('')
+    const [ Cookie,_ ] = useCookies(["auth_token"]);
+    const [Transactions, setTransactions] = useState([])
+
+    const IncomeSchema = z.object({
+        Description: z.string().min(1, 'Description is required'),
+        Amount: z.string().min(1, 'Amount is required'),
+        Date: z.string().min(1, 'Date is required'),
+        Memo: z.string().min(1, 'Memo is required'),
+    });
+
+    type FormData = z.infer<typeof IncomeSchema>;
+
+    const { register, handleSubmit, formState: { errors } } = useForm<FormValues>({
+        resolver: zodResolver(IncomeSchema),
+    });
+
+    // Calculation of Total Income
+
+    const CalculateIncome = () => {
+        let TotalIncome = 0;
+        Transactions.forEach((Transaction) => {
+            TotalIncome += parseInt(Transaction.Amount);
+        });
+        return TotalIncome;
+    };
+
+    // Delete Income Transaction
+
+    const handleDelete= async(_id) => {
+        await Axios.delete(`https://ebudget-server.onrender.com/Income/${_id}`, {
+            headers: {authorization: Cookie.auth_token}
+        }) 
+    }
+
+    const AddIncome: SubmitHandler<FormValues> = async (data: FormData) => {
+        await Axios.post("https://localhost:4000/Income/AddIncome", data, {
+            headers: { authorization: Cookie.auth_token },
+        }) 
+        console.log(data)
+        setSuccess('Income details has been successfully added.') 
+    };
+
+    useEffect(() => {
+
+        const FetchIncome = () => {
+            Axios.get(`https://ebudget-server.onrender.com/Income/${userID}/Incomes`, {
+            headers: { authorization: Cookie.auth_token },
+            }) 
+            .then((Response) => {
+                setTransactions(Response.data)
+            })
+        } 
+    
+        if (userOwner) {
+            FetchIncome()
+        }
+        },[Transactions])
+
+return (
+    <div className='flex flex-col gap-10 text-black w-11/12'>
+        <Heading
+            Heading='Income'
+            HeadingStyle='font-bold text-center text-green-700 text-5xl'
+            TransactionStyle='bg-blue-950 font-bold flex items-center justify-center h-20 rounded text-4xl text-white text-center'
+            TransactionName='Total Income'
+        />
+        <div className='flex gap-5'>
+        <section className='flex flex-col items-center mb-5 px-5'>
+            <form method="post" onSubmit={handleSubmit(AddIncome)} encType="multipart/form-data" className='flex flex-col gap-8'>
+                <div className='flex flex-col gap-2'>
+                    <label className='font-bold text-lg' htmlFor="">Income Description</label> 
+                    <input placeholder="Income/Revenue Description..." {...register('Description', { required: 'Description is required' })} className='border-black border-b h-8 outline-none truncate px-1 py-1 text-black w-96' required />
+                    {errors.Description && <p className="text-center text-red-700">{errors.Description.message}</p>}
+                </div>
+                <div className='flex flex-col gap-2'>
+                    <label className='font-bold text-lg' htmlFor="">Income Amount</label>
+                    <input className="border-black border-b h-6 outline-none px-1 py-2 w-96"  placeholder="Income/Revenue Amount..." {...register('Amount', { required: 'Income Amount is required' })} required/>
+                    {errors.Amount && <p className="text-center text-red-700">{errors.Amount.message}</p>}
+                </div>
+                <div className='flex flex-col gap-2'>
+                    <label className='font-bold text-lg' htmlFor="">Date</label>
+                    <input type='date' className="border-black border-b h-6 outline-none px-1 py-2 w-96" {...register('Date', { required: 'Date is required' })} required ></input>
+                    {errors.Date && <p className="text-center text-red-700">{errors.Date.message}</p>}
+                </div>
+                <div className='flex flex-col gap-2'>
+                    <label className='font-bold text-lg' htmlFor="">Memo</label>
+                    <textarea placeholder='Add a Memo...' className="border-black border-b h-20 px-1 outline-none py-2 w-96" {...register('Memo', { required: 'Memo is required' })} required ></textarea>
+                    {errors.Memo && <p className="text-center text-red-700">{errors.Memo.message}</p>}
+                </div>
+                <div className='flex flex-col items-center mt-10 text-center'>
+                    <h4 className='font-bold text-center text-green-700'>{Success}</h4>
+                    <Button
+                        ButtonText='Add Income/Revenue'
+                        ButtonStyle='bg-green-800 cursor-pointer flex items-center justify-center h-10 px-3 py-1 rounded-sm text-center text-white w-40'
+                        onClick={handleSubmit(AddIncome)}
+                    />
+                </div>
+            </form>
+        </section>
+        <section className='mt-5 w-full' >
+            <Output
+                Description='Salary'
+                Amount='50,000'
+                Date='10-07-2024'
+                ifMemo='Memo'
+                Memo='Salary for the month of December.'
+            />
+        </section>
+        </div>
+    </div>
+)
+}
+
+export default Income
