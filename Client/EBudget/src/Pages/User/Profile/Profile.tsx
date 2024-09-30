1bb7560e9ed450ebf732e376130148ba78377de9
Eric Kioko <107955102+KiokoEric@users.noreply.github.com>
2024-09-30 17:19:15 +0300

    Modification of the User Profile Page

--- Client/EBudget/src/Pages/User/Profile/Profile.tsx
+++ Client/EBudget/src/Pages/User/Profile/Profile.tsx
@@ -1,27 +1,42 @@
 import axios from 'axios';
 import { useCookies } from "react-cookie";
-import { useParams } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import ProfileImage from "../../../assets/Profile.png";
+import { useNavigate, useParams } from 'react-router-dom';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import Input from '../../../Components/Common/Input/Input';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import Button from '../../../Components/Common/Button/Button';
+import { faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
-
 
 const Profile:React.FC = () => {
 
     const { userID } = useParams()
-    const [Name, setName] = useState("")
-    const [Email, setEmail] = useState("")
-    const [Password, setPassword] = useState("")
+    const navigate = useNavigate()
     const [Cookie, _] = useCookies(["auth_token"]);
+
+    // USESTATE HOOK
+
+    const [Name, setName] = useState<string>("")
+    const [Email, setEmail] = useState<string>("")
+    const [Password, setPassword] = useState<string>("")
+    const [showPassword, setShowPassword] = useState<boolean>(false)
+
+    // DISPLAYING AND HIDING OF PASSWORDS
+
+    const handleTogglePassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    // CALLING ON THE USER'S DETAILS
 
     useEffect(() => {
 
         const FetchUser =() => {
         try{
-            axios.get(`https://ebudget-server.onrender.com/Users/${userID}`, {
+            axios.get(`http://localhost:4000/Users/${userID}`, {
             headers: { authorization: Cookie.auth_token },
             }) 
             .then((Data) => { 
@@ -39,33 +54,64 @@ const Profile:React.FC = () => {
         
     }, [])
 
+    // EDIT USER FUNCTION
+
+    const EditUser = () => {
+        navigate(`/EditProfile/${userID}`)
+    } 
+
+    // DELETE USER FUNCTION
+
+    const DeleteUser = () => {
+        navigate(`/DeleteProfile`)
+    }
+
 return (
-    <div className='flex flex-col items-center justify-center m-auto w-full'>
-        <form method="post" encType="multipart/form-data" className='bg-green-800 flex flex-col items-center gap-5 px-5 py-5 rounded text-white'>
-            <FontAwesomeIcon icon={faUser} className='bg-black -mt-20 px-9 py-8 rounded-full text-8xl text-white' />
-            <h1 className='font-bold pt-5 text-center text-5xl'>Profile</h1>
-            <div className='flex flex-col gap-2'>
-                <label className='text-lg' htmlFor="">Name</label> 
-                <input className='border-black border-b h-8 outline-none rounded-sm truncate px-1 py-4 text-black text-lg w-96' value={Name} required />
+    <div className='flex sm:grid sm:grid-cols-2 gap-5 items-center justify-center mt-1'>
+        <figure className='hidden md:block' >
+            <img src={ProfileImage} alt="" />
+        </figure>
+        <form encType="multipart/form-data" className='flex flex-col items-center justify-start gap-2 text-lg'>
+            <div className='mb-10'>
+                <h2 className='text-5xl'><span className='text-green-800'>E</span>Budget</h2>
+                <p className='mt-5 text-xl text-center'>My Profile</p>
             </div>
-            <div className='flex flex-col gap-2'>
-                <label className='text-lg' htmlFor="Email">Email</label> 
-                <input className='border-black border-b h-8 outline-none rounded-sm truncate px-1 py-4 text-black text-lg w-96' value={Email} required />
-            </div>
-            <div className='flex flex-col gap-2'>
-                <label className='text-lg' htmlFor="Password">Password</label> 
-                <input className='border-black border-b h-8 outline-none rounded-sm truncate px-1 py-4 text-black text-lg w-96' value={Password} required />
-            </div>
-            <div className='flex gap-10'>
+            <Input 
+                ContainerStyle = 'flex flex-col gap-1'
+                Label = 'Name'
+                LabelStyle = 'font-bold'
+                inputStyle = 'border-black border-b h-5 outline-none truncate px-1 py-2 text-black w-80 sm:w-96' 
+                Value={Name}        
+            />
+            <Input 
+                ContainerStyle = 'flex flex-col gap-1'
+                Label = 'Email'
+                LabelStyle = 'font-bold'
+                inputStyle = 'border-black border-b h-5 outline-none truncate px-1 py-2 text-black w-80 sm:w-96'
+                Value={Email}
+            />
+            <Input 
+                ContainerStyle = 'flex flex-col w-80 sm:w-96'
+                Label = 'Password'
+                type={showPassword ? 'text' : 'password'}
+                LabelStyle = 'font-bold'
+                inputStyle = 'h-5 outline-none truncate px-1 py-2 text-black w-80 sm:w-96'
+                TextStyle="border-black border-b flex flex-row"
+                Value={Password}
+                Children={showPassword ? <FontAwesomeIcon icon={faEye} className="underline" onClick={handleTogglePassword} /> : <FontAwesomeIcon icon={faEyeSlash} className="underline" onClick={handleTogglePassword} />  }
+            />
+            <div className='flex gap-5 mt-5 sm:gap-16'>
                 <Button
+                    onClick={EditUser}
+                    ButtonStyle="bg-black cursor-pointer flex items-center justify-center gap-4 text-center text-white px-2 py-1.5 rounded w-40"
+                    ButtonText="Edit Details"
                     Children={<FontAwesomeIcon icon={faPenToSquare} />}
-                    ButtonText='Edit Details'
-                    ButtonStyle='bg-black cursor-pointer flex gap-2 items-center justify-center mt-1 text-center text-lg text-white px-3 py-2 rounded w-36'
                 />
                 <Button
+                    onClick={DeleteUser}
+                    ButtonStyle="bg-black cursor-pointer flex items-center justify-center gap-4 text-center text-white px-2 py-1.5 rounded w-40"
+                    ButtonText="Delete My Profile"
                     Children={<FontAwesomeIcon icon={faTrash} />}
-                    ButtonText='Delete Profile'
-                    ButtonStyle='bg-black cursor-pointer flex gap-2 items-center justify-center mt-1 text-center text-lg text-white px-3 py-2 rounded w-36'
                 />
             </div>
         </form>
