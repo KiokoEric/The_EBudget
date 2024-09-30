e3930bba3cad341ad791ceed49925d20ff1750bb
Eric Kioko <107955102+KiokoEric@users.noreply.github.com>
2024-09-30 15:20:23 +0300

    Creation of reusable Navigate Component

---
+++ Client/EBudget/src/Components/Common/Navigate/Navigate.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+interface NavigateProps {
+    onClick?:any
+    children?: any;
+    Navigation?: any;
+    NavigateText: string;
+    NavigateStyle: string;
+}
+
+const Navigate: React.FC<NavigateProps> = ({ Navigation, NavigateStyle, children, NavigateText, onClick }) => {
+return (
+    <Link to={Navigation} className={NavigateStyle} onClick={onClick}>
+        { children }
+        { NavigateText }
+    </Link>
+)
+}
+
+export default React.memo(Navigate)
