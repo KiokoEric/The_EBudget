a019dba26f4dadbad4eb8479bf36b7398744a337
Eric Kioko <107955102+KiokoEric@users.noreply.github.com>
2024-09-30 16:06:58 +0300

    Modification of the Labels component

--- Client/EBudget/src/Components/Labels/Labels.tsx
+++ Client/EBudget/src/Components/Labels/Labels.tsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 const Labels: React.FC = () => {
 return (
-    <div className='flex flex-col gap-2' >
-        <h3 className='font-bold text-2xl'>Labels</h3>
+    <div className='flex flex-col gap-2 items-center sm:items-start'>
+        <h3 className='font-bold text-center text-2xl'>Labels</h3>
         <section className='flex gap-2'>
             <div id='Indicator' className='bg-yellow-500 m-1.5'></div>
             <p className='text-lg'>Total Savings</p>
         </section>
         <section className='flex gap-2'>
-            <div id='Indicator' className='bg-blue-800 m-1.5' ></div>
+            <div id='Indicator' className='bg-blue-500 m-1.5' ></div>
             <p className='text-lg'>Total Balance</p>
         </section>
         <section className='flex gap-2'>
-            <div id='Indicator' className='bg-red-700 m-1.5' ></div>
+            <div id='Indicator' className='bg-pink-500 m-1.5' ></div>
             <p className='text-lg'>Total Expenses</p>
         </section>
     </div>
