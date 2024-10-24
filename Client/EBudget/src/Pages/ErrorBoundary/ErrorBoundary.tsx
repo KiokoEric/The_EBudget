17f5e8d89d076f0cbfb6dbe2eaa10b40b1bf4e6e
Eric Kioko <107955102+KiokoEric@users.noreply.github.com>
2024-10-24 20:26:00 +0300

    Update of the Error Boundary Page

--- Client/EBudget/src/Pages/ErrorBoundary/ErrorBoundary.tsx
+++ Client/EBudget/src/Pages/ErrorBoundary/ErrorBoundary.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ErrorBoundary: React.FC = () => {
 return (
     <div className='m-auto w-screen'>
-        <h1 className='font-bold mt-5 text-red-600 text-3xl text-center w-screen'>Kindly Login to access the EBudget Resources.</h1>
+        <h1 className='font-bold mt-5 text-green-800 text-3xl text-center w-screen'>Kindly Login to access the EBudget Resources.</h1>
     </div>
     
 )
